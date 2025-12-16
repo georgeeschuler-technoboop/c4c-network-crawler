56f099e2a83eebb6b59faea00f5f861293883aca
georgeeschuler-technoboop <george.e.schuler@gmail.com>
2025-12-15 19:44:57 -0500

    Update __init__.py
    board extractor addition

--- c4c_utils/__init__.py
+++ c4c_utils/__init__.py
@@ -1,7 +1,7 @@
 # C4C Utilities Package
 # Network analysis utilities for Connecting for Change
-
 from . import irs990_parser
 from . import network_export
+from . import board_extractor
 
 __version__ = "0.1.0"
