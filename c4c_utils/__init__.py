b46364d74bf3962ef6ad5d97313349054152bfa5
georgeeschuler-technoboop <george.e.schuler@gmail.com>
2025-12-17 20:59:21 -0500

    Update __init__.py
    added
    
    from . import region_helpers
    from . import grants_schema

--- c4c_utils/__init__.py
+++ c4c_utils/__init__.py
@@ -4,5 +4,7 @@ from . import irs990_parser
 from . import network_export
 from . import board_extractor
 from . import irs_return_qa
+from . import region_helpers
+from . import grants_schema
 
 __version__ = "0.1.0"
